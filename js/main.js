3485330fd86b6e84e625e3bb3c04b096a22293eb
Hridanshkhaitan <43408668+Hridanshkhaitan@users.noreply.github.com>
2025-03-20 19:42:22 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Global variable
+let myIconVis;
+
+// Load data
+d3.csv("data/icon-data.csv")
+    .then(function(data) {
+        data.forEach(d => {
+            d.hostingFee = +d.hostingFee;
+            d.ticketPrice = +d.ticketPrice;
+
+        });
+
+        // Initialize the page
+        initMainPage(data);
+    })
+
+function initMainPage(data) {
+
+    // Create an instance of IconVis
+    myIconVis = new IconVis("icon-place", "popup-place", data);
+}
+
+function sortBy(key) {
+    myIconVis.sortBy(key);
+}
