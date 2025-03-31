e9f3423f89291ec53c630cd990a2651d8e7543f3
davatana <david.atanasov@mail.utoronto.ca>
2025-03-31 15:20:59 -0400

    Adds my major

--- js/visualization.js
+++ js/visualization.js
@@ -86,7 +86,7 @@ function wrangleData() {
           const titles = ["Terrence Allder", "David Atanasov", "Hridansh Khaitan"];
           const subtitles = [
             "Math Specialist + CS minor",
-            "Whatever you study",
+            "Bioinformatics & CS",
             "Whatever you study"
           ];
           
